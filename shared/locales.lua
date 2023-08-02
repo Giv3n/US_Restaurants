4e20e4966df81f09523adbbc1e764dfd2efad098
Mr.G <39389889+Giv3n@users.noreply.github.com>
2023-08-02 16:13:40 +0200

    Update locales.lua
    New locale

--- shared/locales.lua
+++ shared/locales.lua
@@ -2,6 +2,7 @@ Locales = {
     ['Main'] = { 
         ['TakeOrder'] = 'Take your order',
         ['MissingMoney'] = "You're missing $%s to pay this order",
-        ["OrderPreparing"] = "Your order is being prepared, you will be asked to pick it up in a moment!"
+        ["OrderPreparing"] = "Your order is being prepared, you will be asked to pick it up in a moment!",
+        ["NoSpaceInInventory"] = "No space in the inventory"
     }
   }
